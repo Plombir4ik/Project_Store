342334c16f457675c23380bfa633f851bbcd65f9
Plombir4ik <plybomur720@gmail.com>
2021-01-19 10:38:11 +0200

    Поміняв ввійти на вбійти

--- Project_Store/Window1.xaml.cs
+++ Project_Store/Window1.xaml.cs
@@ -23,5 +23,10 @@ namespace Project_Store
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
